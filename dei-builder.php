53b5c614167e60cb7ed8b775634301b9df57030b
Cayden Simler <simler.cayden@gmail.com>
2022-02-13 13:25:22 -0800

    added DEI Builder template

---
+++ dei-builder.php
@@ -0,0 +1,18 @@
+<?php
+// Template Name: DEI Builder
+get_header();
+
+if (function_exists('get_field')) {
+	// Content Wrapper
+	if (get_post_meta(get_the_ID(), 'content_wrapper_classes', true)) {
+		echo '<div class="' . get_post_meta(get_the_ID(), 'content_wrapper_classes', true) . '">';
+			// Page Content
+			get_template_part('acf/clones/page-content');
+		echo '</div>';
+	} else {
+		// Page Content
+		get_template_part('acf/clones/page-content');
+	}
+}
+
+get_footer();
